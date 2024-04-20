4fbd753d61424fed2785829ed81dab7bcf02c354
Gian Franco <gian_franco_08@hotmail.com>
2024-04-19 23:47:59 -0500

    remove ','

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,13 +1,12 @@
-import {writable, } from 'svelte/store'
-import {browser} from '$app/environment'
+import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
-
-const initialValue = browser ? (localStorage.getItem('theme') || 'light') : 'light'
+const initialValue = browser ? localStorage.getItem('theme') || 'light' : 'light';
 
 export const theme = writable<string>(initialValue);
 
 theme.subscribe((value) => {
-    if(browser){
-        localStorage.setItem('theme', value)
-    }
-})
+	if (browser) {
+		localStorage.setItem('theme', value);
+	}
+});
