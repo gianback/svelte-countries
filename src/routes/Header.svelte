24af311dfdff1dab86a87d2e70f444d256f6ee12
Gian Franco <gian_franco_08@hotmail.com>
2024-04-21 02:33:35 -0500

    feat: add theme store

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,12 +1,21 @@
 <script>
 	import Moon from "$lib/icons/Moon.svelte";
+	import Sun from "$lib/icons/Sun.svelte";
+	import {theme} from "../store/theme"
+
+	$: currentTheme = $theme === 'light' ? 'color: red' : 'color: blue'
 
 </script>
-<header>
-	<h1>
+<header class="flex items-center justify-between">
+	
+	<h1 style={currentTheme}>
 		Where in the world?
 	</h1>
-	<button>
-		<Moon />
+	<button class="flex items-center">
+		{#if $theme === 'light' }
+		Light mode <Sun />
+		{:else}
+		Dark mode<Moon />
+		{/if}
 	</button>
 </header>
--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,12 +1,13 @@
-import { writable } from 'svelte/store';
+import { writable, } from 'svelte/store';
 import { browser } from '$app/environment';
 
 const initialValue = browser ? localStorage.getItem('theme') || 'light' : 'light';
 
 export const theme = writable<string>(initialValue);
 
-theme.subscribe((value) => {
+ theme.subscribe((value) => {
 	if (browser) {
 		localStorage.setItem('theme', value);
 	}
 });
+
